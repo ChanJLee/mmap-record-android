f9048290248b69489f93b812ddc7be29343df42a
ChanJLee <1355425625@qq.com>
2018-01-21 19:04:48 +0800

    sync

---
+++ lib/src/main/java/com/chan/lib/MmapRecord.java
@@ -0,0 +1,9 @@
+package com.chan.lib;
+
+/**
+ * Created by chan on 2018/1/21.
+ */
+
+public class MmapRecord {
+	public native void init();
+}
---
+++ lib/src/main/jni/mmaprecord.cpp
@@ -0,0 +1,8 @@
+#include <jni.h>
+
+JNIEXPORT void JNICALL
+Java_com_chan_lib_MmapRecord_init(JNIEnv *env, jobject instance) {
+
+    // TODO
+
+}
